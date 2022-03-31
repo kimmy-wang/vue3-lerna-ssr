1ce4bb5b36ba48014cdf5578ce5a0734b2f8088e
see <upcwangying@126.com>
2022-03-31 17:54:24 +0800

    refactor

--- packages/home/src/App.vue
+++ packages/home/src/App.vue
@@ -1,28 +1,15 @@
 <template>
-  <video-background :sources="['/video/timelapse.mp4']" img="/img/bg.jpeg">
-    <!-- Our Nav Bar Component and Side Menu -->
-    <nav-bar />
-    <!-- Main Content and Buttons -->
-    <intro-section />
-    <!-- Footer -->
-    <footer-section />
-  </video-background>
+  <hello />
 </template>
 
 <script lang="ts">
 // Import our components from component directory
-import VideoBackground from "./components/VideoBackground.vue";
-import NavBar from "./components/Nav/NavBar.vue";
-import IntroSection from "./components/Intro/IntroSection.vue";
-import Footer from "./components/Footer.vue";
+import Hello from "@/components/hello.vue";
 
 // Use our components in our App.vue template
 export default {
   components: {
-    IntroSection,
-    NavBar,
-    FooterSection: Footer,
-    VideoBackground,
+    Hello,
   },
 };
 </script>
